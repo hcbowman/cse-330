bc408f6df6a953398ae3790a3d02ef23a6e1be44
Hunter Bowman <hcbowman@asu.edu>
2024-09-16 21:24:52 +0000

    test commit

--- project2/kernel_module/my_name.c
+++ project2/kernel_module/my_name.c
@@ -1,2 +1,18 @@
 #include <linux/init.h>
-#in
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Hunter Bowman");
+
+static int __init example_init(void) {
+    printk(KERN_INFO "Hello World\n");
+    return 0;
+}
+
+static void __exit example_exit(void) {
+    printk(KERN_INFO "Goodbye World\n");
+}
+
+module_init(example_init);
+module_init(example_exit);
